657dd35f02cfdec338bfdec7523c1ba8bc0e3005
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-26 15:08:39 +0100

    0.1.5

--- QXUYPWGPQO/__main__.py
+++ QXUYPWGPQO/__main__.py
@@ -4,10 +4,9 @@ Dummy
 """
 import elib
 
-
 def main():
     from QXUYPWGPQO import __version__
-    print(__version__)
+    print(__version__, type(__version__))
     elib.updater.Updater('132nd-etcher/hello_world', __version__, 'QXUYPWGPQO.exe').update()
 
 
