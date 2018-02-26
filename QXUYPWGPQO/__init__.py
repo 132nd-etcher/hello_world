ebf1ff4f3c8645bcbf152c38b5fadeb48100f018
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-26 15:07:42 +0100

    fix version

--- QXUYPWGPQO/__init__.py
+++ QXUYPWGPQO/__init__.py
@@ -8,10 +8,10 @@ import elib
 from pkg_resources import DistributionNotFound, get_distribution
 
 try:
-    __version__ = get_distribution('emft').version
+    __version__: str = get_distribution('emft').version
 except DistributionNotFound:  # pragma: no cover
     if getattr(sys, 'frozen', False):
-        __version__ = elib.exe_version.get_product_version(sys.executable)
+        __version__: str = elib.exe_version.get_product_version(sys.executable).file_version
     else:
         # package is not installed
-        __version__ = 'not installed'
+        __version__: str = 'not installed'
