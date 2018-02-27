fe66c15abba0df9a07ba8efcdff2c565d35a7991
132nd-etcher <132nd-etcher@daribouca.net>
2018-02-27 05:57:39 +0100

    uh

--- test/test_imports.py
+++ test/test_imports.py
@@ -9,27 +9,28 @@ import glob
 
 import pytest
 
-import epab
+import pvnhxgmgiq
 
 
 @pytest.fixture(autouse=True)
 def patch_version(monkeypatch):
-    monkeypatch.setattr(epab, '__version__', '0.0.0')
+    monkeypatch.setattr(pvnhxgmgiq, '__version__', '0.0.0')
 
 
 # noinspection PyUnresolvedReferences,PyProtectedMember
 @pytest.mark.nocleandir
-@pytest.mark.parametrize('module', glob.glob('./pvnhxgmgiq/*.py', recursive=True))
+@pytest.mark.parametrize('module', glob.glob('pvnhxgmgiq/*.py', recursive=True))
 def test_imports(module):
-    module = module[2:-3].replace('\\', '.')
+    # print(module[2:-3])
+    module = module[:-3].replace('\\', '.')
     print(f'importing {module}')
     __import__(module)
 
 
 # noinspection PyUnresolvedReferences,PyProtectedMember
 @pytest.mark.nocleandir
-@pytest.mark.parametrize('module', list(glob.glob('./pvnhxgmgiq/*.py', recursive=True)))
+@pytest.mark.parametrize('module', list(glob.glob('pvnhxgmgiq/*.py', recursive=True)))
 def test_imports_tests(module):
-    module = module[2:-3].replace('\\', '.')
+    module = module[:-3].replace('\\', '.')
     print(f'importing {module}')
     __import__(module)
